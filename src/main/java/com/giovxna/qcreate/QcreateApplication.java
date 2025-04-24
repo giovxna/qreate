948538c4d135a9848bcafe421ce2a4e88093a88e
giovxna <gi.siqueira0202@gmail.com>
2025-04-24 16:28:53 -0300

    chore: add initial project dependencies (zxing e awssdk)

---
+++ src/main/java/com/giovxna/qcreate/QcreateApplication.java
@@ -0,0 +1,13 @@
+package com.giovxna.qcreate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QcreateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QcreateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/giovxna/qcreate/QcreateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.giovxna.qcreate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QcreateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
